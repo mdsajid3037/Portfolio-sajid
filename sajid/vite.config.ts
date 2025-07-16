2bd290af278bc85af1cbae1c7bce33f5079ff1b3
mdsajid3037 <mdasjid3037@gmail.com>
2025-07-17 00:20:09 +0530

    fix packages

--- sajid/vite.config.ts
+++ sajid/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [ react()].filter(Boolean),
-  base:process.env.BASE_VITE_PATH || "/Portfolio-sajid",
+  plugins: [react()],
+  // Removed or simplified `base` for Vercel
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
